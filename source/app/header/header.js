e7a347d0c8424e3239d23ccec57c907de7abaca8
Josh Graber <josh.graber.85@gmail.com>
2016-12-01 17:52:17 -0500

    Use ui link for header anchor

--- source/app/header/header.js
+++ source/app/header/header.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
+import { Link } from 'react-router'
 import ProfileContainer from '../profile/profile-container'
 
 const Header = () => {
   return (
     <header>
-      <a href="/" className="title">
+      <Link to="/" className="title">
         <i className="fa fa-car"></i>
         <span>Mileage</span>
-      </a>
+      </Link>
       <ProfileContainer />
     </header>
   )
